6a0e901e4ae3adbc954dc023d1ab091030677d17
Adel Alkawmani <adel.alkawmani86@gmail.com>
2022-10-01 14:53:53 +0200

    Inialization_project

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+# intialize new api instance
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# intialize our web server
